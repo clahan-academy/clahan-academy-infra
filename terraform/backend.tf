362efe6368952d21e98988268db09c591e442bc8
Vignesh <91901@example.com>
2026-06-18 23:17:02 +0530

    refactor: remove unused service components and associated configuration files

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-clahan-academy-global"
-    storage_account_name = "stclahantfstate"
-    container_name       = "tfstate"
-    key                  = "clahan-academy.tfstate"
-    use_azuread_auth     = false
-  }
-}
--- terraform/modules/monitoring/outputs.tf
+++
@@ -1,1 +0,0 @@
-# Outputs for monitoring module are empty as resources are managed in root main.tf.
