f96a17a1d9fe4f26e88a518beb44df184e4bf90e
Vignesh <91901@example.com>
2026-06-22 15:18:58 +0530

    fix: remove secret imports since they were successfully deleted in Key Vault

--- imports.tf
+++ imports.tf
@@ -6,3 +6,5 @@ import {
   to = module.aks.azurerm_kubernetes_cluster.main
   id = "/subscriptions/65bf2554-8090-4538-9c38-8a6e9c5f6f22/resourceGroups/rg-clahan-prod/providers/Microsoft.ContainerService/managedClusters/aks-clahan-academy"
 }
+
+
