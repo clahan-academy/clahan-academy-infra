7e47f05c6b00cc358f5d2e235c318dca9f65a5cd
Vignesh <91901@example.com>
2026-06-22 16:11:30 +0530

    feat: add AKS module with managed identity, node pools, and RBAC configuration

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -117,7 +117,4 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   principal_id                     = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
   scope                            = var.acr_id
   skip_service_principal_aad_check = true
-  lifecycle {
-    ignore_changes = all
-  }
 }
